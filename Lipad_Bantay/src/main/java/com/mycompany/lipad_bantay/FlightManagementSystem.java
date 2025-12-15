aa56b1026e7fc0bebd970c7a7341be43fe0f5208
joshua-arcos22 <joshuaolivercomot.arcos24@bicol-u.edu.ph>
2025-12-15 14:47:52 +0800

    Chanegs on the databse

--- Lipad_Bantay/src/main/java/com/mycompany/lipad_bantay/FlightManagementSystem.java
+++ Lipad_Bantay/src/main/java/com/mycompany/lipad_bantay/FlightManagementSystem.java
@@ -1,3 +1,4 @@
+package Lipad_Bantay.src.main.java.com.mycompany.lipad_bantay;
 public class FlightManagementSystem {
   public static void main (String args[]) {
     System.out.print("If you're reading this, you are gay!!!");
---
+++ Lipad_Bantay/target/classes/com/mycompany/lipad_bantay/README.md
@@ -0,0 +1,2 @@
+# Flight Management System
+Flight Management System as a Final Project for the courses OOP and DSA, created by Arcos, Balin, Gerona, Tanguin and Valenciano
