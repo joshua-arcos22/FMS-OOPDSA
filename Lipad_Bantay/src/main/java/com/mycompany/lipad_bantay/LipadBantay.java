de0bc393782ed717d05b8f96c90a863a0ca8e2c4
joshua-arcos22 <joshuaolivercomot.arcos24@bicol-u.edu.ph>
2025-12-15 14:28:42 +0800

    UI

---
+++ Lipad_Bantay/src/main/java/com/mycompany/lipad_bantay/LipadBantay.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lipad_bantay;
+
+/**
+ *
+ * @author Joshua
+ */
+public class LipadBantay {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Lipad_Bantay/src/main/java/com/mycompany/lipad_bantay/Lipad_Bantay.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lipad_bantay;
+
+/**
+ *
+ * @author Joshua
+ */
+public class Lipad_Bantay {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
